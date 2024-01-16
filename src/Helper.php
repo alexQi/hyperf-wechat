622315c6aa0d3ca47329ed5ddc88691dc4a074b0
{alex.qiubo@qq.com} <alex.qiubo@qq.com>
2024-01-16 22:12:52 +0800

    update: 修复适配hyperf高版本

--- src/Helper.php
+++ src/Helper.php
@@ -13,7 +13,7 @@ declare(strict_types=1);
 namespace Naixiaoxin\HyperfWechat;
 
 use Hyperf\HttpMessage\Stream\SwooleStream;
-use Hyperf\Utils\Context;
+use Hyperf\Context\Context;
 use Psr\Http\Message\ResponseInterface as PsrResponseInterface;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -22,7 +22,9 @@ class Helper
     public static function Response(Response $response)
     {
         $psrResponse = Context::get(PsrResponseInterface::class);
-        $psrResponse = $psrResponse->withBody(new SwooleStream($response->getContent()))->withStatus($response->getStatusCode());
+        $psrResponse = $psrResponse->withBody(new SwooleStream($response->getContent()))->withStatus(
+            $response->getStatusCode()
+        );
         foreach ($response->headers->all() as $key => $item) {
             $psrResponse = $psrResponse->withHeader($key, $item);
         }
